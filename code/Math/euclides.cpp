a49a9a106dcbe632a0fa85c37341002e89ec0c97
gabrielpessoa1 <gap@cin.ufpe.br>
2018-05-21 14:38:59 -0300

    Logaritmo Discreto e Euclides Estendido

---
+++ code/Math/euclides.cpp
@@ -0,0 +1,25 @@
+// euclides para gcd:
+
+ll gcd(ll a, ll b) {
+	while(b) a %= b, swap(a, b);
+	return a;
+}
+
+// euclides estendido: acha u e v da equacao:
+// u * x + v * y = gcd(x, y);
+// u eh inverso modular de x no modulo y
+// v eh inverso modular de y no modulo x
+
+pair<ll, ll> euclides(ll a, ll b) {
+	ll u = 0, oldu = 1, v = 1, oldv = 0;
+	while(b) {
+		ll q = a / b;
+		oldv = oldv - v * q;
+		oldu = oldu - u * q;
+		a = a - b * q;
+		swap(a, b);
+		swap(u, oldu);
+		swap(v, oldv);
+	}
+	return make_pair(oldu, oldv);
+}
