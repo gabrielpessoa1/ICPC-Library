a098b1f26f8a11197f16d2d9057e0009c655cf9b
Gabriel Pessoa <gap@cin.ufpe.br>
2018-06-11 02:34:56 -0300

    Simplificação do Código de Intersection Points

--- code/Geometry/IntersectionPoints.cpp
+++ code/Geometry/IntersectionPoints.cpp
@@ -1,19 +1,15 @@
 int intersectionPoints(vector<pair<PT, PT>> v) {
     int n = v.size();
-    vector<pair<int, int> events;
-    vector<pair<int, int>> vertInt;
-    // tipo -1 = Inicio de segmento horizontal
-    // tipo inf = Fim de segmento horizontal
-    // tipo natural = Segmento vertical
+    vector<pair<int, int> events, vertInt;
     for(int i = 0; i < n; i++) {
-        if(v.first.x == v.second.x) {
+        if(v.first.x == v.second.x) { // Segmento Vertical
             int y0 = min(v.first.y, v.second.y), y1 = max(v.first.y, v.second.y);
-            events.push_back({v.first.x, vertInt.size()});
+            events.push_back({v.first.x, vertInt.size()}); // Tipo = Indice no array
             vertInt.push_back({y0, y1});
-        } else {
+        } else { // Segmento Horizontal
             int x0 = min(v.first.x, v.second.x), x1 = max(v.first.x, v.second.x);
-            events.push_back({x0, -1});
-            events.push_back({x1, inf});
+            events.push_back({x0, -1}); // Inicio de Segmento
+            events.push_back({x1, inf}); // Final de Segmento
         }
     }
     sort(events.begin(), events.end());
