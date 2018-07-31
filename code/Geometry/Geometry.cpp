9805331e7d30ac2e1ccf0d699bceac36af3e2714
bcs5 <33490486+bcs5@users.noreply.github.com>
2018-07-31 15:12:38 -0300

    Update Geometry.cpp

--- code/Geometry/Geometry.cpp
+++ code/Geometry/Geometry.cpp
@@ -2,6 +2,7 @@ const double inf = 1e100, eps = 1e-9;
 
 struct PT {
 	double x, y;
+	PT(){}
 	PT(double x, double y) : x(x), y(y) {}
 	PT operator + (const PT &p) { return PT(x + p.x, y + p.y); }
 	PT operator - (const PT &p) { return PT(x - p.x, y - p.y); }
