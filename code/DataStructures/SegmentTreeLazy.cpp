01ab8639a3bebd15c44c7c718d5784440d16080c
Gabriel Pessoa <gap@cin.ufpe.br>
2018-07-31 21:02:31 -0300

    Usar metodo para propagar

--- code/DataStructures/SegmentTreeLazy.cpp
+++ code/DataStructures/SegmentTreeLazy.cpp
@@ -10,8 +10,7 @@ void build(int idx = 0, int l = 0, int r = n - 1) {
     seg[idx] = seg[left] + seg[right];
 }
 
-int query(int L, int R, int idx = 0, int l = 0, int r = n - 1) {
-    int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
+void propagate(int idx, int l, int r, int left, int right) {
     if(lazy[idx]) {
         seg[idx] += lazy[idx] * (r - l + 1);
         if(l < r) {
@@ -19,7 +18,12 @@ int query(int L, int R, int idx = 0, int l = 0, int r = n - 1) {
             lazy[right] += lazy[idx];
         }
         lazy[idx] = 0;
-    }
+    }    
+}
+
+int query(int L, int R, int idx = 0, int l = 0, int r = n - 1) {
+    int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
+    propagate(idx, l, r, left, right);
     if(R < l || L > r) return 0;
     if(L <= l && r <= R) return seg[idx];
     return query(L, R, left, l, mid) + query(L, R, right, mid + 1, r);
@@ -27,17 +31,11 @@ int query(int L, int R, int idx = 0, int l = 0, int r = n - 1) {
 
 void update(int V, int L, int R, int idx = 0, int l = 0, int r = n -1) {
     int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
-    if(lazy[idx]) {
-        seg[idx] += lazy[idx] * (r - l + 1);
-        if(l < r) {
-            lazy[left] += lazy[idx];
-            lazy[right] += lazy[idx];
-        }
-        lazy[idx] = 0;
-    }
+    propagate(idx, l, r, left, right);
     if(l > R || r < L) return;
     if(L <= l && r <= R) {
-        lazy[idx] += V;
+        lazy[idx] += V;        
+        propagate(idx, l, r, left, right);
         return;
     }
     update(V, L, R, left, l, mid); update(V, L, R, right, mid + 1, r);
