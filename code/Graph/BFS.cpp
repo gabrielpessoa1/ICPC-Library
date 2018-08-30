6013c048b80c8a08cc21fb115a20d284048ed47b
gabrielpessoa1 <gap@cin.ufpe.br>
2018-08-30 01:28:13 -0300

    BFS simplificado

--- code/Graph/BFS.cpp
+++ code/Graph/BFS.cpp
@@ -1,15 +1,19 @@
-int dis[ms], q[ms], front, rear;
+vector<int> g[ms];
+int dis[ms];
+queue<int> q;
 
 void bfs(int x) {
-    memset(dis, -1, sizeof dis);
-    dis[x] = 0;	front = 0; rear = 0;
-    q[rear++] = x;
-    while(front < rear) {
-        int v = q[front++];
-        for(int i = adj[v]; i > -1; i = ant[i]) {
-            if(~dis[to[i]]) continue;
-            dis[to[i]] = dis[v] + 1;
-            q[rear++] = to[i];
-        }
+    memset(dis, 63, sizeof dis);
+    dis[x] = 0;
+    q.push(x);
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for(int v : g[u]) {
+            if(dis[v] > dis[u] + 1) {
+                dis[v] = dis[u] + 1;
+                q.push(v);
+            }
+       }
     }
 }
