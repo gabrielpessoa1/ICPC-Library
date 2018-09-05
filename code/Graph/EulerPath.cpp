daf13901c603e3dbf1832b0376fb01bdd0932e9c
gabrielpessoa1 <gap@cin.ufpe.br>
2018-09-05 00:59:18 -0300

    Caminho euleriano simplificado e ampliado para guardar arestas

--- code/Graph/EulerPath.cpp
+++ code/Graph/EulerPath.cpp
@@ -1,13 +1,13 @@
-int path[me], sz, del[me];
+int pathV[me], szV, del[me], pathE, szE;
 int adj[ms], to[me], ant[me], wt[me], z, n;
 
 // Funcao de add e clear no dinic
 
 void eulerPath(int u) {
-    for(int i = adj[u]; i > -1; i = ant[u]) {
-        if(del[i]) continue;
-        del[i] = 1; del[i^1] = 1;
+    for(int i = adj[u]; ~i; i = ant[u]) if(!del[i]) {
+        del[i] = del[i^1] = 1;
         eulerPath(to[i]);
+        pathE[szE++] = i;
     }
-    path[sz++] = v;
+    pathV[szV++] = u;
 }
