436043734e4f6c0aee87c2fad21f4daf02f303b9
gabrielpessoa1 <gap@cin.ufpe.br>
2018-09-10 20:53:17 -0300

    Seg com lazy agora limpa lazy no build

--- code/DataStructures/SegmentTreeLazy.cpp
+++ code/DataStructures/SegmentTreeLazy.cpp
@@ -2,6 +2,7 @@ int arr[4 * ms],  seg[4 * ms], lazy[4 * ms], n;
 
 void build(int idx = 0, int l = 0, int r = n - 1) {
     int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
+    lazy[idx] = 0;
     if(l == r) {
         seg[idx] = arr[l];
         return;
@@ -10,12 +11,13 @@ void build(int idx = 0, int l = 0, int r = n - 1) {
     seg[idx] = seg[left] + seg[right];
 }
 
-void propagate(int idx, int l, int r, int left, int right) {
+void propagate(int idx, int l, int r) {
+    int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
     if(lazy[idx]) {
         seg[idx] += lazy[idx] * (r - l + 1);
         if(l < r) {
-            lazy[left] += lazy[idx];
-            lazy[right] += lazy[idx];
+            lazy[2*idx+1] += lazy[idx];
+            lazy[2*idx+2] += lazy[idx];
         }
         lazy[idx] = 0;
     }    
@@ -23,7 +25,7 @@ void propagate(int idx, int l, int r, int left, int right) {
 
 int query(int L, int R, int idx = 0, int l = 0, int r = n - 1) {
     int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
-    propagate(idx, l, r, left, right);
+    propagate(idx, l, r);
     if(R < l || L > r) return 0;
     if(L <= l && r <= R) return seg[idx];
     return query(L, R, left, l, mid) + query(L, R, right, mid + 1, r);
@@ -31,11 +33,11 @@ int query(int L, int R, int idx = 0, int l = 0, int r = n - 1) {
 
 void update(int V, int L, int R, int idx = 0, int l = 0, int r = n -1) {
     int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
-    propagate(idx, l, r, left, right);
+    propagate(idx, l, r);
     if(l > R || r < L) return;
     if(L <= l && r <= R) {
         lazy[idx] += V;        
-        propagate(idx, l, r, left, right);
+        propagate(idx, l, r);
         return;
     }
     update(V, L, R, left, l, mid); update(V, L, R, right, mid + 1, r);
