d0adebefa6ea06d1b0d541cda9983c2cf521410f
gabrielpessoa1 <gap@cin.ufpe.br>
2018-09-19 21:24:50 -0300

    Determinante de matriz com mod do chines

---
+++ code/Math/DetMod.cpp
@@ -0,0 +1,43 @@
+// by zchao1995
+// Determinante com coordenadas inteiras usando Mod
+
+ll mat[ms][ms];
+
+ll det (int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      mat[i][j] %= mod;
+    }
+  }
+  ll res = 1;
+  for (int i = 0; i < n; i++) {
+    if (!mat[i][i])	{
+      bool flag = false;
+      for (int j = i + 1; j < n; j++) {
+        if (mat[j][i]) {
+          flag = true;
+          for (int k = i; k < n; k++) {
+            swap (mat[i][k], mat[j][k]);
+          }
+          res = -res;
+          break;
+        }
+      }
+      if (!flag) {
+        return 0;
+      }
+    }
+    for (int j = i + 1; j < n; j++) {
+      while (mat[j][i]) {
+        ll t = mat[i][i] / mat[j][i];
+        for (int k = i; k < n; k++) {
+          mat[i][k] = (mat[i][k] - t * mat[j][k]) % mod;
+          swap (mat[i][k], mat[j][k]);
+        }
+        res = -res;
+      }
+    }
+    res = (res * mat[i][i]) % mod;
+  }
+  return (res + mod) % mod;
+}
