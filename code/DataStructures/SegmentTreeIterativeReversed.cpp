7e7441e7b44f2ae989936692f9fa33b1b6249753
gabrielpessoa1 <gap@cin.ufpe.br>
2018-09-20 17:31:35 -0300

    Modificado ordem no update da seg ite rev

--- code/DataStructures/SegmentTreeIterativeReversed.cpp
+++ code/DataStructures/SegmentTreeIterativeReversed.cpp
@@ -4,10 +4,10 @@ void build() {
   for(int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
 }
 
-void update(int l, int r, int value) {
+void update(int v, int l, int r) {
   for(l += n, r += n+1; l < r; l >>= 1, r >>= 1) {
-    if(l&1) t[l++] += value;
-    if(r&1) t[--r] += value;
+    if(l&1) t[l++] += v;
+    if(r&1) t[--r] += v;
   }
 }
 
