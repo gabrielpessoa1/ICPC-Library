1e6c58edae3cbb83ce7e2a04315de91dc14af9f8
Gabriel Pessoa <gap@cin.ufpe.br>
2018-09-29 00:27:20 -0300

    Adicionado comentario ordenacao topologica no dfs

--- code/Graph/DFS.cpp
+++ code/Graph/DFS.cpp
@@ -1,6 +1,7 @@
 vector<int> g[ms];
 int vis[ms], topo[ms], topoLen;
 
+// DFS fazendo uma ordenacao topologica
 void dfs(int u) {
   if(vis[u]) return;
   vis[u] = true;
@@ -8,7 +9,7 @@ void dfs(int u) {
   topo[topoLen++] = u;
 }
 
-void dfs() {
+void solve() {
   memset(vis, 0, sizeof vis);
   for(int i = 0; i < n; i++) dfs(i);
   reverse(topo, topo + n);
