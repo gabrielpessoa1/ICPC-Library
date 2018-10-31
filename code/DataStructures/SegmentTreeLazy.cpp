e7b02ee612ab201fa26f10ac1e10bad8a7cde818
Gabriel Pessoa <gap@cin.ufpe.br>
2018-10-31 00:17:13 -0300

    Corrigido acentos na lazy

--- code/DataStructures/SegmentTreeLazy.cpp
+++ code/DataStructures/SegmentTreeLazy.cpp
@@ -8,18 +8,18 @@ void build(int idx = 0, int l = 0, int r = n - 1) {
     return;
   }
   build(left, l, mid); build(right, mid + 1, r);
-  seg[idx] = seg[left] + seg[right]; // merge
+  seg[idx] = seg[left] + seg[right]; // Merge
 }
 
 void propagate(int idx, int l, int r) {
   int mid = (l+r)/2, left = 2 * idx + 1, right = 2 * idx + 2;
   if(lazy[idx]) {
-    seg[idx] += lazy[idx] * (r - l + 1); // aplicar lazy no seg
+    seg[idx] += lazy[idx] * (r - l + 1); // Aplicar lazy no seg
     if(l < r) {
-      lazy[2*idx+1] += lazy[idx];  // merge de lazy
-      lazy[2*idx+2] += lazy[idx];  // merge de lazy
+      lazy[2*idx+1] += lazy[idx];  // Merge de lazy
+      lazy[2*idx+2] += lazy[idx];  // Merge de lazy
     }
-    lazy[idx] = 0; // limpar a lazy
+    lazy[idx] = 0; // Limpar a lazy
   }  
 }
 
@@ -28,7 +28,7 @@ int query(int L, int R, int idx = 0, int l = 0, int r = n - 1) {
   propagate(idx, l, r);
   if(R < l || L > r) return 0; // Valor que nao atrapalhe
   if(L <= l && r <= R) return seg[idx];
-  return query(L, R, left, l, mid) + query(L, R, right, mid + 1, r); // merge
+  return query(L, R, left, l, mid) + query(L, R, right, mid + 1, r); // Merge
 }
 
 void update(int V, int L, int R, int idx = 0, int l = 0, int r = n -1) {
@@ -36,10 +36,10 @@ void update(int V, int L, int R, int idx = 0, int l = 0, int r = n -1) {
   propagate(idx, l, r);
   if(l > R || r < L) return;
   if(L <= l && r <= R) {
-    lazy[idx] += V; // merge de lazy/ou so colocar
+    lazy[idx] += V; // Merge de lazy/ou so colocar
     propagate(idx, l, r);
     return;
   }
   update(V, L, R, left, l, mid); update(V, L, R, right, mid + 1, r);
-  seg[idx] = seg[left] + seg[right]; // merge
+  seg[idx] = seg[left] + seg[right]; // Merge
 }
