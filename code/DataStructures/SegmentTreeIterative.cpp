376e06dbb0f10a43d9dbd9eb65e5bfe6438c2d7c
Gabriel Pessoa <gap@cin.ufpe.br>
2018-10-31 00:13:10 -0300

    Adicionado comentarios na seg iterativa

--- code/DataStructures/SegmentTreeIterative.cpp
+++ code/DataStructures/SegmentTreeIterative.cpp
@@ -1,18 +1,18 @@
 int n, t[2 * ms];
 
 void build() {
-  for(int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+  for(int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1]; // Merge
 }
 
 void update(int p, int value) { // set value at position p
-  for(t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+  for(t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1]; // Merge
 }
 
 int query(int l, int r) {
   int res = 0;
   for(l += n, r += n+1; l < r; l >>= 1, r >>= 1) {
-    if(l&1) res += t[l++];
-    if(r&1) res += t[--r];
+    if(l&1) res += t[l++]; // Merge
+    if(r&1) res += t[--r]; // Merge
   }
   return res;
 }
