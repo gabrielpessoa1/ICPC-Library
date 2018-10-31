ed1fa3336c36cbc096521c37af9ee00f515b5fec
Gabriel Pessoa <gap@cin.ufpe.br>
2018-10-31 00:14:50 -0300

    Adicionado comentarios na iterativa reversa

--- code/DataStructures/SegmentTreeIterativeReversed.cpp
+++ code/DataStructures/SegmentTreeIterativeReversed.cpp
@@ -1,26 +1,26 @@
 int n, t[2 * ms];
 
 void build() {
-  for(int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+  for(int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1]; // Merge
 }
 
 void update(int v, int l, int r) {
   for(l += n, r += n+1; l < r; l >>= 1, r >>= 1) {
-    if(l&1) t[l++] += v;
-    if(r&1) t[--r] += v;
+    if(l&1) t[l++] += v; // Merge
+    if(r&1) t[--r] += v; // Merge
   }
 }
 
 int query(int p) {
   int res = 0;
-  for(p += n; p > 0; p >>= 1) res += t[p];
+  for(p += n; p > 0; p >>= 1) res += t[p]; // Merge
   return res;
 }
 
 void push() { // push modifications to leafs
   for(int i = 1; i < n; i++) {
-    t[i<<1] += t[i];
-    t[i<<1|1] += t[i];
+    t[i<<1] += t[i]; // Merge
+    t[i<<1|1] += t[i]; // Merge
     t[i] = 0;
   }
 }
