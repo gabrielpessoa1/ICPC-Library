539f4877c2673f5925e9e212a1b5c9afd3e637c3
gabrielpessoa1 <gap@cin.ufpe.br>
2018-11-07 02:49:42 -0300

    Knuth optimization added

---
+++ code/DP/Knuth.cpp
@@ -0,0 +1,18 @@
+int n, m, mid[ms][ms];
+ll dp[ms][ms];
+
+void knuth() {
+  for(int i = n; i >= 0; i--) { // limites entre 0 e n
+    dp[i][i+1] = 0; mid[i][i+1] = i; // intervalo de tamanho 1 nao diminuem
+    for(int j = i+2; j <= n; j++) {
+      dp[i][j] = inf; // long long inf
+      for(int k = mid[i][j-1]; k <= mid[i+1][j]; k++) {
+        if(dp[i][j] > dp[i][k] + dp[k][j]) {
+          dp[i][j] = dp[i][k] + dp[k][j];
+          mid[i][j] = k;
+        }
+      }
+      dp[i][j] += c(i, j); // custo associado ao intervalo 
+    }
+  }
+}
