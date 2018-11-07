133cbddb59d2b2e1a434df4210093106cfc7f859
gabrielpessoa1 <gap@cin.ufpe.br>
2018-11-07 02:52:43 -0300

    Comentario modificado no knuth

--- code/DP/Knuth.cpp
+++ code/DP/Knuth.cpp
@@ -3,7 +3,7 @@ ll dp[ms][ms];
 
 void knuth() {
   for(int i = n; i >= 0; i--) { // limites entre 0 e n
-    dp[i][i+1] = 0; mid[i][i+1] = i; // intervalo de tamanho 1 nao diminuem
+    dp[i][i+1] = 0; mid[i][i+1] = i; // caso base
     for(int j = i+2; j <= n; j++) {
       dp[i][j] = inf; // long long inf
       for(int k = mid[i][j-1]; k <= mid[i+1][j]; k++) {
