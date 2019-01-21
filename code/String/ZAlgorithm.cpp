a9dafd9216d6833c97633ba030d889f03875a25f
Gabriel Pessoa <gap@cin.ufpe.br>
2019-01-21 13:14:59 -0200

    Update ZAlgorithm.cpp

--- code/String/ZAlgorithm.cpp
+++ code/String/ZAlgorithm.cpp
@@ -4,12 +4,11 @@ int fz[ms], n;
 void zfunc() {
   fz[0] = n;
   for(int i = 1, l = 0, r = 0; i < n; i++) {
-    if(l &&  i + fz[i-l] < r)
-      fz[i] = fz[i-l];
-    else {
-      int j = min(l ? fz[i-l] : 0, i > r ? 0 : r - i);
-      while(s[i+j] == s[j] && ++j);
-      fz[i] = j; l = i; r = i + j;
+    fz[i] = max(0, min(r-i, fz[i-l]));
+    while(s[fz[i]] == s[i+fz[i]]) ++fz[i];
+    if(i + fz[i] > r) {
+      l = i;
+      r = i + fz[i];
     }
   }
 }
