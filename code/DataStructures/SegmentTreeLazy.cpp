61f944c6c695b33c1ea62e3fa2267e989ef24942
Gabriel Pessoa <gap@cin.ufpe.br>
2019-08-28 01:56:20 -0300

    Update SegmentTreeLazy.cpp

--- code/DataStructures/SegmentTreeLazy.cpp
+++ code/DataStructures/SegmentTreeLazy.cpp
@@ -21,7 +21,7 @@ int query(int L, int R, int idx = 0, int l = 0, int r = n-1) {
     }
     lazy[idx] = 0; // Limpar a lazy
   }
-  if(R < l || L > r) return 0; // Valor que nao atrapalhe
+  if(l > R || r < L) return 0; // Valor que nao atrapalhe
   if(L <= l && r <= R) return seg[idx];
   return query(L, R, 2*idx+1, l, mid) + query(L, R, 2*idx+2, mid+1, r); // Merge
 }
