11711dc0161384156b19ec4a69b964050f364e81
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-09-07 20:39:58 -0300

    Create LineContainer.cpp

---
+++ code/DataStructures/LineContainer.cpp
@@ -0,0 +1,34 @@
+typedef long long int ll;
+
+bool Q;
+struct Line {
+  mutable ll k, m, p;
+  bool operator<(const Line& o) const {
+    return Q ? p < o.p : k < o.k;
+  }
+};
+
+struct LineContainer : multiset<Line> {
+  // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+  const ll inf = LLONG_MAX;
+  ll div(ll a, ll b) { // floored division
+    return a / b - ((a ^ b) < 0 && a % b); }
+  bool isect(iterator x, iterator y) {
+    if (y == end()) { x->p = inf; return false; }
+    if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+    else x->p = div(y->m - x->m, x->k - y->k);
+    return x->p >= y->p;
+  }
+  void add(ll k, ll m) {
+    auto z = insert({k, m, 0}), y = z++, x = y;
+    while (isect(y, z)) z = erase(z);
+    if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+    while ((y = x) != begin() && (--x)->p >= y->p)
+      isect(x, erase(y));
+  }
+  ll query(ll x) {
+    assert(!empty());
+    Q = 1; auto l = *lower_bound({0,0,x}); Q = 0;
+    return l.k * x + l.m;
+  }
+};
