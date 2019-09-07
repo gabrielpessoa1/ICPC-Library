92f01c0d66b4ba2eed9751997c37b855507ce340
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-09-07 20:20:58 -0300

    fix identation

--- code/Graph/HLD.cpp
+++ code/Graph/HLD.cpp
@@ -1,91 +1,91 @@
 // src: tfg
 class HLD {
 public:
-	void init(int n) {
-		// this doesn't delete edges!
-		sz.resize(n);
-		in.resize(n);
-		out.resize(n);
-		rin.resize(n);
-		p.resize(n);
-		edges.resize(n);
-		nxt.resize(n);
-		h.resize(n);
-	}
+  void init(int n) {
+    // this doesn't delete edges!
+    sz.resize(n);
+    in.resize(n);
+    out.resize(n);
+    rin.resize(n);
+    p.resize(n);
+    edges.resize(n);
+    nxt.resize(n);
+    h.resize(n);
+  }
 
-	void addEdge(int u, int v) {
-		edges[u].push_back(v);
-		edges[v].push_back(u);
-	}
+  void addEdge(int u, int v) {
+    edges[u].push_back(v);
+    edges[v].push_back(u);
+  }
 
-	void setRoot(int n) {
-		t = 0;
-		p[n] = n;
-		h[n] = 0;
-		prep(n, n);
-		nxt[n] = n;
-		hld(n);
-	}
+  void setRoot(int n) {
+    t = 0;
+    p[n] = n;
+    h[n] = 0;
+    prep(n, n);
+    nxt[n] = n;
+    hld(n);
+  }
 
-	int getLCA(int u, int v) {
-		while(!inSubtree(nxt[u], v)) {
-			u = p[nxt[u]];
-		}
-		while(!inSubtree(nxt[v], u)) {
-			v = p[nxt[v]];
-		}
-		return in[u] < in[v] ? u : v;
-	}
+  int getLCA(int u, int v) {
+    while(!inSubtree(nxt[u], v)) {
+      u = p[nxt[u]];
+    }
+    while(!inSubtree(nxt[v], u)) {
+      v = p[nxt[v]];
+    }
+    return in[u] < in[v] ? u : v;
+  }
 
-	bool inSubtree(int u, int v) {
-		// is v in the subtree of u?
-		return in[u] <= in[v] && in[v] < out[u];
-	}
+  bool inSubtree(int u, int v) {
+    // is v in the subtree of u?
+    return in[u] <= in[v] && in[v] < out[u];
+  }
 
-	vector<pair<int, int>> getPathtoAncestor(int u, int anc) {
-		// returns ranges [l, r) that the path has
-		vector<pair<int, int>> ans;
-		//assert(inSubtree(anc, u));
-		while(nxt[u] != nxt[anc]) {
-			ans.emplace_back(in[nxt[u]], in[u] + 1);
-			u = p[nxt[u]];
-		}
-		// this includes the ancestor!
-		ans.emplace_back(in[anc], in[u] + 1);
-		return ans;
-	}
+  vector<pair<int, int>> getPathtoAncestor(int u, int anc) {
+    // returns ranges [l, r) that the path has
+    vector<pair<int, int>> ans;
+    //assert(inSubtree(anc, u));
+    while(nxt[u] != nxt[anc]) {
+      ans.emplace_back(in[nxt[u]], in[u] + 1);
+      u = p[nxt[u]];
+    }
+    // this includes the ancestor!
+    ans.emplace_back(in[anc], in[u] + 1);
+    return ans;
+  }
 private:
-	vector<int> in, out, p, rin, sz, nxt, h;
-	vector<vector<int>> edges;
-	int t;
+  vector<int> in, out, p, rin, sz, nxt, h;
+  vector<vector<int>> edges;
+  int t;
 
-	void prep(int on, int par) {
-		sz[on] = 1;
-		p[on] = par;
-		for(int i = 0; i < (int) edges[on].size(); i++) {
-			int &u = edges[on][i];
-			if(u == par) {
-				swap(u, edges[on].back());
-				edges[on].pop_back();
-				i--;
-			} else {
-				h[u] = 1 + h[on];
-				prep(u, on);
-				sz[on] += sz[u];
-				if(sz[u] > sz[edges[on][0]]) {
-					swap(edges[on][0], u);
-				}
-			}
-		}
-	}
+  void prep(int on, int par) {
+    sz[on] = 1;
+    p[on] = par;
+    for(int i = 0; i < (int) edges[on].size(); i++) {
+      int &u = edges[on][i];
+      if(u == par) {
+        swap(u, edges[on].back());
+        edges[on].pop_back();
+        i--;
+      } else {
+        h[u] = 1 + h[on];
+        prep(u, on);
+        sz[on] += sz[u];
+        if(sz[u] > sz[edges[on][0]]) {
+          swap(edges[on][0], u);
+        }
+      }
+    }
+  }
 
-	void hld(int on) {
-		in[on] = t++;
-		rin[in[on]] = on;
-		for(auto u : edges[on]) {
-			nxt[u] = (u == edges[on][0] ? nxt[on] : u);
-			hld(u);
-		}
-		out[on] = t;
-	}
+  void hld(int on) {
+    in[on] = t++;
+    rin[in[on]] = on;
+    for(auto u : edges[on]) {
+      nxt[u] = (u == edges[on][0] ? nxt[on] : u);
+      hld(u);
+    }
+    out[on] = t;
+  }
 };
