5aab3a1ce3e3d0c7369c89559c3bc03268bfb57b
gabrielpessoa1 <gap@cin.ufpe.br>
2019-09-08 14:08:03 -0300

    beats seg tree

--- code/DataStructures/SegmentTreeLazy.cpp
+++ code/DataStructures/SegmentTreeLazy.cpp
@@ -11,20 +11,26 @@ void build(int idx = 0, int l = 0, int r = n-1) {
   seg[idx] = seg[2*idx+1] + seg[2*idx+2]; // Merge
 }
 
-void propagate(int idx, int l, int r) 
-  if(lazy[idx]) {
-    seg[idx] += lazy[idx] * (r - l + 1); // Aplicar lazy no seg
+void apply(int idx, int l, int r) {
+  int mid = (l+r)/2;
+  if(lazy[idx] && !canBreak) { // if not beats canBreak = false
     if(l < r) {
       lazy[2*idx+1] += lazy[idx];  // Merge de lazy
       lazy[2*idx+2] += lazy[idx];  // Merge de lazy
     }
-    lazy[idx] = 0; // Limpar a lazy
+    if(canApply) { // if not beats canApply = true
+      seg[idx] += lazy[idx] * (r - l + 1); // Aplicar lazy no seg
+    } else {
+      apply(2*idx+1, l, mid); apply(2*idx+2, mid+1, r);
+      seg[idx] = seg[2*idx+1] + seg[2*idx+2]; // Merge
+    }
   }
+  lazy[idx] = 0; // Limpar a lazy
 }
 
 int query(int L, int R, int idx = 0, int l = 0, int r = n-1) {
   int mid = (l+r)/2;
-  propagate(idx, l, r);
+  apply(idx, l, r);
   if(l > R || r < L) return 0; // Valor que nao atrapalhe
   if(L <= l && r <= R) return seg[idx];
   return query(L, R, 2*idx+1, l, mid) + query(L, R, 2*idx+2, mid+1, r); // Merge
@@ -32,11 +38,11 @@ int query(int L, int R, int idx = 0, int l = 0, int r = n-1) {
 
 void update(int L, int R, int V, int idx = 0, int l = 0, int r = n-1) {
   int mid = (l+r)/2;
-  propagate(idx, l, r);
+  apply(idx, l, r);
   if(l > R || r < L) return;
   if(L <= l && r <= R) {
     lazy[idx] = V;
-    propagate(idx, l, r);
+    apply(idx, l, r);
     return;
   }
   update(L, R, V, 2*idx+1, l, mid); update(L, R, V, 2*idx+2, mid+1, r);
