2db5da986835ba726cfe99c0dd30d2b37291922d
gabrielpessoa1 <gap@cin.ufpe.br>
2019-09-08 13:29:54 -0300

    fixed beza's treap

--- code/DataStructures/Treap.cpp
+++ code/DataStructures/Treap.cpp
@@ -1,21 +1,20 @@
 mt19937 rng ((int) chrono::steady_clock::now().time_since_epoch().count());
 
-typedef int Key;
+typedef int Value;
 typedef struct item * pitem;
 
 struct item {
   item () {}
-  item (Key k) {
-    key = k;
+  item (Value v) { // add key if not implicit
+    value = v;
     prio = uniform_int_distribution<int>() (rng);
     cnt = 1;
     rev = 0;
-    l = r = NULL;
+    l = r = 0;
   }
   pitem l, r;
-  Key key;
-  int prio;
-  int cnt;
+  Value value;
+  int prio, cnt;
   bool rev;
 };
 
@@ -29,20 +28,17 @@ void fix (pitem it) {
 }
 
 void pushLazy (pitem it) {
-    if (!it) return;
-    if (it->rev) {
-        it->rev = false;
-        swap (it->l, it->r);
-        if (it->l)  it->l->rev ^= true;
-        if (it->r)  it->r->rev ^= true;
-    }
+  if (it && it->rev) {
+    it->rev = false;
+    swap(it->l, it->r);
+    if (it->l)  it->l->rev ^= true;
+    if (it->r)  it->r->rev ^= true;
+  }
 }
 
 void merge (pitem & t, pitem l, pitem r) {
-  pushLazy (l);
-  pushLazy (r);
-  if (!l || !r)
-    t = l ? l : r;
+  pushLazy (l); pushLazy (r);
+  if (!l || !r) t = l ? l : r;
   else if (l->prio > r->prio)
     merge (l->r, l->r, r),  t = l;
   else
@@ -50,16 +46,24 @@ void merge (pitem & t, pitem l, pitem r) {
   fix (t);
 }
 
-void split (pitem t, pitem & l, pitem & r, int key, int add = 0) {
-  if (!t)
-    return void( l = r = 0 );
+void split (pitem t, pitem & l, pitem & r, int key) {
+  if (!t) return void( l = r = 0 );
   pushLazy (t);
-  int cur_key = add + cnt(t->l);
+  int cur_key = cnt(t->l); // t->key if not implicit
   if (key <= cur_key)
-    split (t->l, l, t->l, key, add),  r = t;
+    split (t->l, l, t->l, key),  r = t;
   else
-    split (t->r, t->r, r, key, add + 1 + cnt(t->l)),  l = t;
+    split (t->r, t->r, r, key - (1 + cnt(t->l))),  l = t;
   fix (t);
+}
+
+void reverse (pitem t, int l, int r) {
+	pitem t1, t2, t3;
+	split (t, t1, t2, l);
+	split (t2, t2, t3, r-l+1);
+	t2->rev ^= true;
+	merge (t, t1, t2);
+	merge (t, t, t3);
 }
 
 void unite (pitem & t, pitem l, pitem r) {
