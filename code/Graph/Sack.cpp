b1c15f02ea1b778839636276fa3a854b97fa5ae5
cfms3 <cfms3@cin.ufpe.br>
2019-09-08 18:58:09 -0300

    Update Sack.cpp

--- code/Graph/Sack.cpp
+++ code/Graph/Sack.cpp
@@ -1,34 +1,27 @@
-void solve(int a, int p, bool f){
-  int big = -1;
-  for(auto &b : adj[a]){
-    if(b != p && (big == -1 || en[b]-st[b] > en[big]-st[big])){
-      big = b;
+void dfs(int v, int par = -1, bool keep = 0) {
+    int big = -1;
+    for (int u : adj[v]) {
+        if (u == par) continue;
+        if (big == -1 || sz[u] > sz[big]) {
+            big = u;
+        }
     }
-  }
-  for(auto &b : adj[a]){
-    if(b == p || b == big) continue;
-    solve(b, a, 0);
-  }
-  if(~big) solve(big, a, 1);
-  add(cnt[v[a]], -1);
-  cnt[v[a]]++;
-  add(cnt[v[a]], +1);
-  for(auto &b : adj[a]){
-    if(b == p || b == big) continue;
-    for(int i = st[b]; i < en[b]; i++){
-      add(cnt[ett[i]], -1);
-      cnt[ett[i]]++;
-      add(cnt[ett[i]], +1);
+    for (int u : adj[v]) {
+        if (u == par || u == big) {
+            continue;
+        }
+        dfs(u, v, 0);
     }
-  }
-  for(auto &q : Q[a]){
-    ans[q.first] = query(mx-1)-query(q.second-1);
-  }
-  if(!f){
-    for(int i = st[a]; i < en[a]; i++){
-      add(cnt[ett[i]], -1);
-      cnt[ett[i]]--;
-      add(cnt[ett[i]], +1);
+    if (big != -1) {
+        dfs(big, v, 1);
     }
-  }
+    for (int u : adj[v]) {
+        if (u == par || u == big) {
+            continue;
+        }
+        put(u, v);
+    }
+    if (!keep) {
+        
+    }
 }
