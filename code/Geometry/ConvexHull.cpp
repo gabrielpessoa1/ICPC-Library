b538556dc6441b7e59401fff38fade154bfd2ea4
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-09-12 23:48:33 -0300

    Update ConvexHull.cpp

--- code/Geometry/ConvexHull.cpp
+++ code/Geometry/ConvexHull.cpp
@@ -1,7 +1,9 @@
-vector<PT> convexHull(vector<PT> p) {
+vector<PT> convexHull(vector<PT> p, bool needs = 1) {
+  if(needs) sort(pts.begin(), pts.end());
+  pts.erase(unique(pts.begin(), pts.end()), pts.end());
   int n = p.size(), k = 0;
-  vector<PT> h(2 * n);
-  sort(p.begin(), p.end());
+  if(n <= 1) return pts;
+  vector<PT> h(n + 2);
   for(int i = 0; i < n; i++) {
     while(k >= 2 && cmp(cross(h[k - 1] - h[k - 2], p[i] - h[k - 2])) <= 0) k--;
     h[k++] = p[i];
@@ -38,4 +40,59 @@ vector<PT> graham (vector<PT> v) {
   }
   u.resize(top);
   return u;
+}
+
+vector<PT> splitHull(const vector<PT> &hull) {
+  vector<PT> ans(hull.size());
+  for(int i = 0, j = (int) hull.size()-1, k = 0; k < (int) hull.size(); k++) {
+    if(hull[i] < hull[j]) {
+      ans[k] = hull[i++];
+    } else {
+      ans[k] = hull[j--];
+    }
+  }
+  return ans;
+}
+
+vector<PT> ConvexHull(const vector<PT> &a, const vector<PT> &b) {
+  auto A = splitHull(a);
+  auto B = splitHull(b);
+  vector<PT> C(A.size() + B.size());
+  merge(A.begin(), A.end(), B.begin(), B.end(), C.begin());
+  return ConvexHull(C, false);
+}
+
+int maximizeScalarProduct(const vector<PT> &hull, PT vec) {
+  // this code assumes that there are no 3 colinear points
+  int ans = 0;
+  int n = hull.size();
+  if(n < 20) {
+    for(int i = 0; i < n; i++) {
+      if(dot(hull[i], vec) > dot(hull[ans], vec)) {
+        ans = i;
+      }
+    }
+  } else {
+    if(dot(hull[1], vec) > dot(hull[ans], vec)) {
+      ans = 1;
+    }
+    for(int rep = 0; rep < 2; rep++) {
+      int l = 2, r = n - 1;
+      while(l != r) {
+        int mid = (l + r + 1) / 2;
+        bool flag = dot(hull[mid], vec) >= dot(hull[mid-1], vec);
+        if(rep == 0) { flag = flag && dot(hull[mid], vec) >= dot(hull[0], vec); }
+        else { flag = flag || dot(hull[mid-1], vec) < dot(hull[0], vec); }
+        if(flag) {
+          l = mid;
+        } else {
+          r = mid - 1;
+        }
+      }
+      if(hull[ans] * vec < hull[l] * vec) {
+        ans = l;
+      }
+    }
+  }
+  return ans;
 }
