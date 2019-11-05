0c830ec3371e30b1e5fb27f089dffe403eb2a75f
cfms3 <cfms3@cin.ufpe.br>
2019-11-05 00:02:47 -0300

    Update MinCostMaxFlow.cpp

--- code/Graph/MinCostMaxFlow.cpp
+++ code/Graph/MinCostMaxFlow.cpp
@@ -1,94 +1,121 @@
-template <class flow_t, class cost_t>
-class MinCostMaxFlow {
-private:
-  typedef pair<cost_t, int> ii;
-
-  struct Edge {
-    int to;
-    flow_t cap;
-    cost_t cost;
-    Edge(int to, flow_t cap, cost_t cost) : to(to), cap(cap), cost(cost) {}
-  };
-
-  int n;
-  vector<vector<int>> adj;
-  vector<Edge> edges;
-  vector<cost_t> dis;
-  vector<int> prev, id_prev;
-  vector<int> q;
-  vector<bool> inq;
-  
-  pair<flow_t, cost_t> spfa(int src, int sink) {
-    fill(dis.begin(), dis.end(), int(1e9)); //cost_t inf
-    fill(prev.begin(), prev.end(), -1);
-    fill(inq.begin(), inq.end(), false);
-    q.clear();
-    q.push_back(src);
-    inq[src] = true;
-    dis[src] = 0;
-    for(int on = 0; on < (int) q.size(); on++) {
-      int cur = q[on];
-      inq[cur] = false;
-      for(auto id : adj[cur]) {
-        if (edges[id].cap == 0) continue;
-        int to = edges[id].to;
-        if (dis[to] > dis[cur] + edges[id].cost) {
-          prev[to] = cur;
-          id_prev[to] = id;
-          dis[to] = dis[cur] + edges[id].cost;
-          if (!inq[to]) {
-            q.push_back(to);
-            inq[to] = true;  
-          }
-        }
-      }
-    }
-    flow_t mn = flow_t(1e9);
-    for(int cur = sink; prev[cur] != -1; cur = prev[cur]) {
-      int id = id_prev[cur];
-      mn = min(mn, edges[id].cap);
-    }
-    if (mn == flow_t(1e9) || mn == 0) return make_pair(0, 0);
-    pair<flow_t, cost_t> ans(mn, 0);
-    for(int cur = sink; prev[cur] != -1; cur = prev[cur]) {
-      int id = id_prev[cur];
-      edges[id].cap -= mn;
-      edges[id ^ 1].cap += mn;
-      ans.second += mn * edges[id].cost;
-    }
-    return ans;
-  }
+template <class T = int>
+class MCMF {
 public:
-  MinCostMaxFlow(int a = 0) {
-    n = a;
-    adj.resize(n + 2);
-    edges.clear();
-    dis.resize(n + 2);
-    prev.resize(n + 2);
-    id_prev.resize(n + 2);
-    inq.resize(n + 2);
-  }
-  void init(int a) {
-    n = a;
-    adj.resize(n + 2);
-    edges.clear();
-    dis.resize(n + 2);
-    prev.resize(n + 2);
-    id_prev.resize(n + 2);
-    inq.resize(n + 2);
-  }
-  void add(int from, int to, flow_t cap, cost_t cost) {
-    adj[from].push_back(int(edges.size()));
-    edges.push_back(Edge(to, cap, cost));
-    adj[to].push_back(int(edges.size()));
-    edges.push_back(Edge(from, 0, -cost));
-  }
-  pair<flow_t, cost_t> maxflow(int src, int sink) {
-    pair<flow_t, cost_t> ans(0, 0), got;
-    while((got = spfa(src, sink)).first > 0) {
-      ans.first += got.first;
-      ans.second += got.second;
-    }
-    return ans;
-  }
+	struct Edge {
+		Edge(int a, T b, T c) : to(a), cap(b), cost(c) {}
+		int to;
+		T cap, cost;
+	};
+
+	MCMF(int size) {
+		n = size;
+		edges.resize(n);
+		pot.assign(n, 0);
+		dist.resize(n);
+		visit.assign(n, false);
+	}
+
+	std::pair<T, T> mcmf(int src, int sink) {
+		std::pair<T, T> ans(0, 0);
+		if(!SPFA(src, sink)) return ans;
+		fixPot();
+		// can use dijkstra to speed up depending on the graph
+		while(SPFA(src, sink)) {
+			auto flow = augment(src, sink);
+			ans.first += flow.first;
+			ans.second += flow.first * flow.second;
+			fixPot();
+		}
+		return ans;
+	}
+
+	void addEdge(int from, int to, T cap, T cost) {
+		edges[from].push_back(list.size());
+		list.push_back(Edge(to, cap, cost));
+		edges[to].push_back(list.size());
+		list.push_back(Edge(from, 0, -cost));
+	}
+private:
+	int n;
+	std::vector<std::vector<int>> edges;
+	std::vector<Edge> list;
+	std::vector<int> from;
+	std::vector<T> dist, pot;
+	std::vector<bool> visit;
+
+	/*bool dij(int src, int sink) {
+		T INF = std::numeric_limits<T>::max();
+		dist.assign(n, INF);
+		from.assign(n, -1);
+		visit.assign(n, false);
+		dist[src] = 0;
+		for(int i = 0; i < n; i++) {
+			int best = -1;
+			for(int j = 0; j < n; j++) {
+				if(visit[j]) continue;
+				if(best == -1 || dist[best] > dist[j]) best = j;
+			}
+			if(dist[best] >= INF) break;
+			visit[best] = true;
+			for(auto e : edges[best]) {
+				auto ed = list[e];
+				if(ed.cap == 0) continue;
+				T toDist = dist[best] + ed.cost + pot[best] - pot[ed.to];
+				assert(toDist >= dist[best]);
+				if(toDist < dist[ed.to]) {
+					dist[ed.to] = toDist;
+					from[ed.to] = e;
+				}
+			}
+		}
+		return dist[sink] < INF;
+	}*/
+
+	std::pair<T, T> augment(int src, int sink) {
+		std::pair<T, T> flow = {list[from[sink]].cap, 0};
+		for(int v = sink; v != src; v = list[from[v]^1].to) {
+			flow.first = std::min(flow.first, list[from[v]].cap);
+			flow.second += list[from[v]].cost;
+		}
+		for(int v = sink; v != src; v = list[from[v]^1].to) {
+			list[from[v]].cap -= flow.first;
+			list[from[v]^1].cap += flow.first;
+		}
+		return flow;
+	}
+
+	std::queue<int> q;
+	bool SPFA(int src, int sink) {
+		T INF = std::numeric_limits<T>::max();
+		dist.assign(n, INF);
+		from.assign(n, -1);
+		q.push(src);
+		dist[src] = 0;
+		while(!q.empty()) {
+			int on = q.front();
+			q.pop();
+			visit[on] = false;
+			for(auto e : edges[on]) {
+				auto ed = list[e];
+				if(ed.cap == 0) continue;
+				T toDist = dist[on] + ed.cost + pot[on] - pot[ed.to];
+				if(toDist < dist[ed.to]) {
+					dist[ed.to] = toDist;
+					from[ed.to] = e;
+					if(!visit[ed.to]) {
+						visit[ed.to] = true;
+						q.push(ed.to);
+					}
+				}
+			}
+		}
+		return dist[sink] < INF;
+	}
+
+	void fixPot() {
+		T INF = std::numeric_limits<T>::max();
+		for(int i = 0; i < n; i++) {
+			if(dist[i] < INF) pot[i] += dist[i];
+		}
+	}
 };
