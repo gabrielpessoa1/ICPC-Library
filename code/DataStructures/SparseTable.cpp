e1b333837b6e0e30abe785e9ffcfa0cef06cc382
cfms3 <cfms3@cin.ufpe.br>
2019-11-05 23:49:00 -0300

    Update SparseTable.cpp

--- code/DataStructures/SparseTable.cpp
+++ code/DataStructures/SparseTable.cpp
@@ -1,37 +1,28 @@
-template<class Info_t>
-class SparseTable {
-private:
-  vector<int> log2;
-  vector<vector<Info_t>> table;
+vector<vector<int>> table;
+vector<int> lg2;
 
-  Info_t merge(Info_t &a, Info_t &b) {
-    
+void build(int n, vector<int> v) {
+  lg2.resize(n + 1);
+  lg2[1] = 0;
+  for (int i = 2; i <= n; i++) {
+    lg2[i] = lg2[i >> 1] + 1;
   }
-
-public:
-  SparseTable(int n, vector<Info_t> v) {
-    log2.resize(n + 1);
-    log2[1] = 0;
-    for (int i = 2; i <= n; i++) {
-      log2[i] = log2[i >> 1] + 1;
-    }
-    table.resize(n + 1);
-    for (int i = 0; i < n; i++) {
-      table[i].resize(log2[n] + 1);
-    }
-    for (int i = 0; i < n; i++) {
-      table[i][0] = v[i];
-    }
-    for (int i = 0; i < log2[n]; i++) {
-      for (int j = 0; j < n; j++) {
-        if (j + (1 << i) >= n) break;
-        table[j][i + 1] = merge(table[j][i], table[j + (1 << i)][i]);
-      }
+  table.resize(lg2[n] + 1);
+  for (int i = 0; i < lg2[n] + 1; i++) {
+    table[i].resize(n + 1);
+  }
+  for (int i = 0; i < n; i++) {
+    table[0][i] = v[i];
+  }
+  for (int i = 0; i < lg2[n]; i++) {
+    for (int j = 0; j < n; j++) {
+      if (j + (1 << i) >= n) break;
+      table[i + 1][j] = min(table[i][j], table[i][j + (1 << i)]);
     }
   }
+}
 
-  int get(int l, int r) {
-    int k = log2[r - l + 1];
-    return merge(table[l][k], table[r - (1 << k) + 1][k]);
-  }
-};
+int get(int l, int r) {
+  int k = lg2[r - l + 1];
+  return min(table[k][l], table[k][r - (1 << k) + 1]);
+}
