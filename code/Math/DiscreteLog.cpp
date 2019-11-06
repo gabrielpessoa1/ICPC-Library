6feef5669096eb17bf04e4c8e9f83749cd63e051
cfms3 <cfms3@cin.ufpe.br>
2019-11-06 00:51:01 -0300

    Update DiscreteLog.cpp

--- code/Math/DiscreteLog.cpp
+++ code/Math/DiscreteLog.cpp
@@ -1,24 +1,20 @@
-ll discreteLog(ll a, ll b, ll m) {
-  // a^ans == b mod m
-  // ou -1 se nao existir
-  ll cur = a, on = 1;
-  for(int i = 0; i < 100; i++) {
-    cur = cur * a % m;
+ll discreteLog (ll a, ll b, ll m) {
+	a %= m; b %= m;
+	ll n = (ll) sqrt (m + .0) + 1, an = 1;
+	for (ll i = 0; i < n; i++) {
+    an = (an * a) % m;
   }
-  while(on * on <= m) {
-    cur = cur * a % m;
-    on++;
-  }
-  map<ll, ll> position;
-  for(ll i = 0, x = 1; i * i <= m; i++) {
-    position[x] = i * on;
-    x = x * cur % m;
-  }
-  for(ll i = 0; i <= on + 20; i++) {
-    if(position.count(b)) {
-      return position[b] - i;
-    }
-    b = b * a % m;
-  }
-  return -1;
+  map<ll, ll> vals;
+	for (ll i = 1, cur = an; i <= n; i++) {
+		if (!vals.count(cur)) vals[cur] = i;
+		cur = (cur * an) % m;
+	}
+  ll ans = 1e18; //inf
+	for (ll i = 0, cur = b; i <= n; i++) {
+		if (vals.count(cur)) {
+      ans = min(ans, vals[cur] * n - i);
+		}
+		cur = (cur * a) % m;
+	}
+	return ans;
 }
