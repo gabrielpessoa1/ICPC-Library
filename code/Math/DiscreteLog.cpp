244aaa99f7ecde6e2cea145e41839b3c324022f2
cfms3 <cfms3@cin.ufpe.br>
2019-11-06 00:52:14 -0300

    Update DiscreteLog.cpp

--- code/Math/DiscreteLog.cpp
+++ code/Math/DiscreteLog.cpp
@@ -1,20 +1,20 @@
 ll discreteLog (ll a, ll b, ll m) {
-	a %= m; b %= m;
-	ll n = (ll) sqrt (m + .0) + 1, an = 1;
-	for (ll i = 0; i < n; i++) {
+  a %= m; b %= m;
+  ll n = (ll) sqrt (m + .0) + 1, an = 1;
+  for (ll i = 0; i < n; i++) {
     an = (an * a) % m;
   }
   map<ll, ll> vals;
-	for (ll i = 1, cur = an; i <= n; i++) {
-		if (!vals.count(cur)) vals[cur] = i;
-		cur = (cur * an) % m;
-	}
+  for (ll i = 1, cur = an; i <= n; i++) {
+    if (!vals.count(cur)) vals[cur] = i;
+    cur = (cur * an) % m;
+  }
   ll ans = 1e18; //inf
-	for (ll i = 0, cur = b; i <= n; i++) {
-		if (vals.count(cur)) {
+  for (ll i = 0, cur = b; i <= n; i++) {
+    if (vals.count(cur)) {
       ans = min(ans, vals[cur] * n - i);
-		}
-		cur = (cur * a) % m;
-	}
-	return ans;
+    }
+    cur = (cur * a) % m;
+  }
+  return ans;
 }
