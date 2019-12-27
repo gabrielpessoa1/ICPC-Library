56a754ca2679f8f5dd2a31451272ac5922585d06
Gabriel Pessoa <gap@cin.ufpe.br>
2019-12-27 12:08:15 -0300

    Update FastExp.cpp

--- code/Math/FastExp.cpp
+++ code/Math/FastExp.cpp
@@ -1,7 +1,7 @@
-const ll mod = 1e9+7;
+const int mod = 1e9+7;
 
-ll fExp(ll a, ll b) {
-  ll ans = 1;
+int fExp(int a, int b) {
+  int ans = 1;
   while(b) {
     if(b & 1) ans = ans * a % mod;
     a = a * a % mod;
