aff6808504f70e5e4bc127c58881fd86aebfa0e9
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2020-01-28 01:48:43 -0300

    Add block cut tree

--- code/Graph/BiconnectedComponents.cpp
+++ code/Graph/BiconnectedComponents.cpp
@@ -1,15 +1,14 @@
 int adj[ms], to[me], ant[me], z;
 int num[ms], low[ms], timer;
-int art[ms], bridge[me], rch;
+bool art[ms], bridge[me], f[me];
 int bc[ms], nbc;
-stack<int> st;
-bool f[me];
+stack<int> st, stk;
+vector<vector<int>> comps;
 
 void clear() { // Lembrar de chamar no main
   memset(adj, -1, sizeof adj);
   z = 0;
 }
-
 
 void add(int u, int v) {
   to[z] = v;
@@ -28,7 +27,7 @@ void generateBc (int v) {
 }
 
 void dfs (int v, int p) {
-  st.push(v);
+  st.push(v), stk.push(v);
   low[v] = num[v] = ++timer;
   for (int i = adj[v]; i != -1; i = ant[i]) {
     if (f[i] || f[i^1]) continue;
@@ -36,11 +35,14 @@ void dfs (int v, int p) {
     int u = to[i];
     if (num[u] == -1) {
       dfs(u, v);
-      if (low[u] > num[v]) bridge[i] = bridge[i^1] = 1;
-      art[v] |= p != -1 && low[u] >= num[v];
-      if (p == -1 && rch > 1) art[v] = 1;
-      else rch ++;
       low[v] = min(low[v], low[u]);
+      if (low[u] > num[v]) bridge[i] = bridge[i^1] = 1; 
+      if (low[u] >= num[v]) {
+        art[v] = (num[v] > 1 || num[u] > 2);
+        comps.push_back({v});
+        while (comps.back().back() != u)
+          comps.back().push_back(stk.top()), stk.pop();
+      }
     } else {
       low[v] = min(low[v], num[u]);
     }
@@ -57,8 +59,27 @@ void biCon (int n) {
   memset(f, 0, sizeof f);
   for (int i = 0; i < n; i++) {
     if (num[i] == -1) {
-      rch = 0;
+      timer = 0;
       dfs(i, 0);
+    }
+  }
+}
+
+vector<int> g[ms];
+int id[ms];
+void buildBlockCut (int n) {
+  int z = 0;
+  for (int u = 0; u < n; ++u) {
+    if (art[u]) id[u] = z++;
+  }
+  for (auto &comp : comps) {
+    int node = z++;
+    for (int u : comp) {
+      if (!art[u]) id[u] = node;
+      else {
+        g[node].push_back(id[u]);
+        g[id[u]].push_back(node);
+      }
     }
   }
 }
