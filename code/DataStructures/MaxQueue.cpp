733a4e4cf77326ed6996d723785a612def9df047
Gabriel Pessoa <gabrielpessoa1904@gmail.com>
2021-09-26 19:02:20 -0300

    Erase empty lines on max queue

--- code/DataStructures/MaxQueue.cpp
+++ code/DataStructures/MaxQueue.cpp
@@ -1,36 +1,25 @@
-// src: tfg50
-template <class T, class C = std::less<T>>
+template <class T, class C = less<T>>
 struct MaxQueue {
-  MaxQueue() {
-    clear();
-  }
-
+  MaxQueue() { clear(); }
   void clear() {
     id = 0;
     q.clear();
   }
-
   void push(T x) {
-    std::pair<int, T> nxt(1, x);
+    pair<int, T> nxt(1, x);
     while(q.size() > id && cmp(q.back().second, x)) {
       nxt.first += q.back().first;
       q.pop_back();
     }
     q.push_back(nxt);
   }
-
-  T qry() {
-    return q[id].second;
-  }
-
+  T qry() { return q[id].second;}
   void pop() {
     q[id].first--;
-    if(q[id].first == 0) {
-      id++;
-    }
+    if(q[id].first == 0) { id++; }
   }
 private:
-  std::vector<std::pair<int, T>> q;
+  vector<std::pair<int, T>> q;
   int id;
   C cmp;
 };
