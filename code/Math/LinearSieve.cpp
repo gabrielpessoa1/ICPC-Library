414b87c978523de2dd4fb3e0efce2661d7d3b67f
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2021-10-26 01:04:17 -0300

    Update LinearSieve.cpp

--- code/Math/LinearSieve.cpp
+++ code/Math/LinearSieve.cpp
@@ -10,26 +10,26 @@ long long getFunction(int i, int p) {
 }
 
 void sieve (int n) {
-	fill(is_composite, is_composite + n, false);
-	func[1] = 1;
-	for (int i = 2; i < n; ++i) {
-		if (!is_composite[i]) {
-			prime.push_back (i);
-			func[i] = 1; // base case
+  fill(is_composite, is_composite + n, false);
+  func[1] = 1;
+  for (int i = 2; i < n; ++i) {
+    if (!is_composite[i]) {
+      prime.push_back (i);
+      func[i] = 1; // base case
       cnt[i] = 1; primePow[i] = i;
-		}
-		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
-			is_composite[i * prime[j]] = true;
-			if (i % prime[j] == 0) { 
-				func[i * prime[j]] = func[i / primePow[i]] * getFunction(i, prime[j]); // f(ip) = f(i / primePow[i]) * f(primePow[i] * prime[j])
-				cnt[i * prime[j]] = cnt[i] + 1;
+    }
+    for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
+      is_composite[i * prime[j]] = true;
+      if (i % prime[j] == 0) { 
+        func[i * prime[j]] = func[i / primePow[i]] * getFunction(i, prime[j]); // f(ip) = f(i / primePow[i]) * f(primePow[i] * prime[j])
+        cnt[i * prime[j]] = cnt[i] + 1;
         primePow[i * prime[j]] = primePow[i] * prime[j];
-				break;
-			} else { 
-				func[i * prime[j]] = func[i] * func[prime[j]]; // f(ip) = f(i) * f(p)
-				cnt[i * prime[j]] = 1;
+        break;
+      } else { 
+        func[i * prime[j]] = func[i] * func[prime[j]]; // f(ip) = f(i) * f(p)
+        cnt[i * prime[j]] = 1;
         primePow[i * prime[j]] = prime[j];
-			}
-		}
-	}
+      }
+    }
+  }
 }
