dbc651f028a0821ac383f4bea6340189e5c839d4
Gabriel Pessoa <gabrielpessoa1904@gmail.com>
2022-03-28 22:49:40 -0300

    change centroid decomp

--- code/Graph/CentroidDecomposition.cpp
+++ code/Graph/CentroidDecomposition.cpp
@@ -1,61 +1,35 @@
-template<typename T>
-struct CentroidDecomposition {
-  vector<int> sz, h, dad;
-  vector<vector<pair<int, T>>> adj;
-  vector<vector<T>> dis;
-  vector<bool> removed;
-  CentroidDecomposition (int n) {
-    sz.resize(n);
-    h.resize(n);
-    dis.resize(n, vector<T>(30, 0));
-    adj.resize(n);
-    removed.resize(n, 0);
-    dad.resize(n);
-  }
-  void add (int a, int b, T w = 1) {
-    adj[a].push_back({b, w});
-    adj[b].push_back({a, w});
-  }
-  void dfsSize (int v, int par){
-    sz[v] = 1;
-    for (auto u : adj[v]){
-      if (u.x == par || removed[u.x]) continue;
-      dfsSize(u.x, v);
-      sz[v] += sz[u.x];
+vector<int> g[ms];
+int par[ms], sz[ms], rem[ms];
+
+void dfsSubtree(int u, int p) {
+  sz[u] = 1;
+  for(auto v : g[u]) {
+    if(v != p && !rem[v]) {
+      dfsSubtree(v, u);
+      sz[u] += sz[v];
     }
   }
-  int getCentroid (int v, int par, int tam){
-    for (auto u : adj[v]) {
-      if (u.x == par || removed[u.x]) continue;
-      if ((sz[u.x]<<1) > tam) return getCentroid(u.x, v, tam);
-    }
-    return v;
+}
+
+int getCentroid(int u, int p, int size) {
+  for(auto v : g[u]) {
+    if(v != p && !rem[v] && sz[v] * 2 >= size) 
+      return getCentroid(v, u, size);
   }
-  void setDis (int v, int par, int nv){
-    for (auto u : adj[v]) {
-      if (u.x == par || removed[u.x]) continue;
-      dis[u.x][nv] = dis[v][nv]+u.y;
-      setDis(u.x, v, nv);
-    }
+  return u;
+}
+
+void decompose(int u, int p) {
+  dfsSubtree(u, -1);
+  int ctr = getCentroid(u, -1, sz[u]);
+  if(p == -1) {
+    p = ctr;
   }
-  void decompose (int v, int par = -1, int nv = 0){
-    dfsSize(v, par);
-    int c = getCentroid(v, par, sz[v]);
-    dad[c] = par;
-    removed[c] = 1;
-    h[c] = nv;
-    setDis(c, par, nv);
-    for (auto u : adj[c]){
-      if (!removed[u.x]){
-        decompose(u.x, c, nv + 1);
-      }
+  par[ctr] = p;
+  rem[ctr] = 1;
+  for(auto v : g[ctr]) {
+    if(v != p && !rem[v]) {
+      decompose(v, ctr);
     }
   }
-  int operator [] (const int idx) const {
-    return dad[idx];
-  }
-  T dist (int u, int v) {
-    if (h[u] < h[v]) swap(u, v);
-    return dis[u][h[v]];
-  }
-};
+}
