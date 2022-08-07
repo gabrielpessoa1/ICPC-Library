0aca9c6aba3d6f6edb8fac4916def621937c46c5
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2022-08-07 01:55:41 -0300

    Create 3dkinho.cpp
    to kinho

---
+++ code/Geometry/3dkinho.cpp
@@ -0,0 +1,22 @@
+struct PT {
+  double x, y, z;
+  PT (double x = 0, double y = 0, double z = 0):x(x),y(y),z(z){};
+  PT operator - (const PT other) {
+    return PT(x - other.x, y - other.y, z - other.z);
+  }
+  bool operator == (const PT other) {
+    PT norm = *this - other;
+    return (abs(norm.y) < 1e-9 && abs(norm.y) < 1e-9 && abs(norm.x) < 1e-9);
+  }
+};
+
+PT cross(PT a, PT b) {
+  return PT(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
+}
+
+double dot (PT a, PT b) {
+  return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+// same plane (a, b, c, p) = dot(cross((a-b), (c-b), p) + d == 0
+// same line (a, b, c) = cross((a-b), (c-b)) == PT(0, 0, 0)
