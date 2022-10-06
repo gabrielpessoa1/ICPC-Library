ace0e498d80c672d488c7d7625ab228f73d68626
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2022-10-06 00:42:04 -0300

    Fix bit2d

--- code/DataStructures/BIT2D.cpp
+++ code/DataStructures/BIT2D.cpp
@@ -1,24 +1,17 @@
-int bit[ms][ms], n, m;
-
+int bit[ms][ms];
 void update(int v, int x, int y) {
-  while(x <= n) {
-    int yTemp = y;
-    while(yTemp <= m) {
-      bit[x][yTemp] += v;
-      yTemp += yTemp&-yTemp;
+  for (x++, y++; x < ms; x += x&-x) {
+    for (int y1 = y+1; y1 < ms; y1 += y1&-y1) {
+      bit[x][y1] += v;
     }
-    x += x&-x;
   }
 }
-
 int query(int x, int y) {
   int r = 0;
-  while(x > 0) {
-    while(y > 0) {
-      r += bit[x][y];
-      y -= y&-y;   
+  for (x++, y++; x > 0; x -= x&-x) {
+    for (int y1 = y; y1 > 0; y1 -= y1&-y1) {
+      r += bit[x][y1];
     }
-    x -= x&-x;
   }
   return r;
 }
