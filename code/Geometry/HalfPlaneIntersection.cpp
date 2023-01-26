91195a896df7805201fe89c546be53246c77b216
Ícaro Julião Monteiro Guerra <ijmg@cin.ufpe.br>
2023-01-26 20:30:05 -0300

    Update HalfPlaneIntersection.cpp

--- code/Geometry/HalfPlaneIntersection.cpp
+++ code/Geometry/HalfPlaneIntersection.cpp
@@ -1,25 +1,33 @@
-struct L {
-    PT a, b;
+struct L { // salvar (i, j) CCW, (j, i) CW
+    PT a, b, dir;
     L(){}
-    L(PT a, PT b) : a(a), b(b) {}
+    L(PT a, PT b) : a(a), b(b) {
+        dir = b - a;
+    }
+    int quadrant() const {
+        if (dir.y > 0 && dir.x >= 0) return 0;
+        if (dir.x < 0 && dir.y >= 0) return 1;
+        if (dir.y < 0 && dir.x <= 0) return 2;
+        return 3;
+    }
+    bool operator < (const L &l) const {
+        int q1 = quadrant(), q2 = l.quadrant();
+        if (q1 != q2) return q1 < q2;
+        return cross(dir, l.dir) > 0;
+    }
 };
-double angle (L la) { return atan2(-(la.a.y - la.b.y), la.b.x - la.a.x); }
-bool comp (L la, L lb) {       
-    if (cmp(angle(la), angle(lb)) == 0) return cross((lb.b - lb.a), (la.b - lb.a)) > eps;
-    return cmp(angle(la), angle(lb)) < 0;
-}
 PT computeLineIntersection (L la, L lb) {
-    return computeLineIntersection(la.a, la.b, lb.a, lb.b);
+    return lineLine(la.a, la.b, lb.a, lb.b);
 }
 bool check (L la, L lb, L lc) {
     PT p = computeLineIntersection(lb, lc);
     double det = cross((la.b - la.a),  (p - la.a));
     return cmp(det) < 0;
 }
-vector<PT> hpi (vector<L> line) { // salvar (i, j) CCW, (j, i) CW
-    sort(line.begin(), line.end(), comp);
+vector<PT> hpi (vector<L> line) {
+    sort(line.begin(), line.end());
     vector<L> pl(1, line[0]);
-    for (int i = 0; i < (int)line.size(); ++i) if (cmp(angle(line[i]), angle(pl.back())) != 0) pl.push_back(line[i]);
+    for (int i = 0; i < (int)line.size(); ++i) if (cmp(cross(line[i].dir, pl.back().dir)) != 0) pl.push_back(line[i]);
     deque<int> dq;
     dq.push_back(0);
     dq.push_back(1);
